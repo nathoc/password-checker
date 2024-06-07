638e68bacac330328341f20f77a334d6b5177ae6
Nathan Cohen <64199013+nathoc@users.noreply.github.com>
2024-06-07 16:51:09 +0100

    Removing code line

--- password_check.py
+++ password_check.py
@@ -44,6 +44,3 @@ hint_password(password_input)
 # ending with question to exit 
 continue_now = input("Do you want to exit? Hit enter. ")
 print("Thanks for using the Password Checker tool. ")
-
-# adding a line of code
-# test 2
